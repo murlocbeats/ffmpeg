4a7a22323a62ad0ab6aed46f77fc8709d80cc19e
murlocbeats <murlocbeats2@gmail.com>
2024-08-03 16:05:33 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask, send_file, abort
 import subprocess
 import os
@@ -38,3 +39,35 @@ def edit_video():
 
 if __name__ == '__main__':
     app.run()
+'''
+
+from flask import Flask, request, send_file
+import subprocess
+import os
+
+app = Flask(__name__)
+
+FFMPEG_PATH = './bin/ffmpeg'
+VIDEO_FILE = 'video.mp4'
+OUTPUT_FILE = 'output.mp4'
+
+@app.route('/process', methods=['GET'])
+def process_video():
+    # Define the command to cut the first 10 seconds and resize the video
+    command = [
+        FFMPEG_PATH,
+        '-i', VIDEO_FILE,
+        '-ss', '00:00:00',
+        '-t', '00:00:10',
+        '-vf', 'scale=360:360',
+        OUTPUT_FILE
+    ]
+    
+    # Run the command
+    subprocess.run(command, check=True)
+    
+    # Return the processed video file
+    return send_file(OUTPUT_FILE, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
